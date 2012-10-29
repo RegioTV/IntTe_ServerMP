0f57a5676394a298c13f48d7be73b86a778080a9
Stefan Derungs <inet@regiotv.ch>
2012-10-29 10:03:48 +0100

    not ManagedBean

--- src/ch/hsr/intte/servermp/RoomBean.java
+++ src/ch/hsr/intte/servermp/RoomBean.java
@@ -1,10 +1,5 @@
 package ch.hsr.intte.servermp;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-
-@ManagedBean
-@SessionScoped
 public class RoomBean {
 
 	public RoomBean(String line) {
