f7315262c3e64d2cb393f143eaa892fa29090c29
Stefan Derungs <inet@regiotv.ch>
2012-11-06 14:19:04 +0100

    Grundstruktur

--- src/ch/hsr/intte/servermp/RoomManager.java
+++ src/ch/hsr/intte/servermp/RoomManager.java
@@ -1,10 +1,49 @@
 package ch.hsr.intte.servermp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+
 import ch.hsr.intte.servermp.model.Room;
-import ch.hsr.intte.servermp.model.User;
 
 public class RoomManager {
 
+	private Set<Room> rooms;
+	
+	public RoomManager() {
+		initialize();
+	}
+
+	private void initialize() {
+		try {
+			rooms = new TreeSet<Room>();
+			File file = new File("room-list");
+			file.createNewFile();
+
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = reader.readLine();
+			while (line != null) {
+				rooms.add(new Room(line));
+				line = reader.readLine();
+			}
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean isRoomnameUnique(String name) {
+		return false;
+	}
+	
 	public Room createRoom(String name) {
 		return null;
 	}
@@ -13,11 +52,11 @@ public class RoomManager {
 		return false;
 	}
 	
-	public boolean enterRoom(User user) {
-		return false;
+	public Room getRoom(String room) {
+		return null;
 	}
 	
-	public boolean leaveRoom(User user) {
-		return false;
+	public Collection<Room> getAllRooms() {
+		return null;
 	}
 }
--- src/ch/hsr/intte/servermp/UserManager.java
+++ src/ch/hsr/intte/servermp/UserManager.java
@@ -1,10 +1,45 @@
 package ch.hsr.intte.servermp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 import ch.hsr.intte.servermp.model.User;
 
 public class UserManager {
 
-	public boolean usernameIsUnique(String username) {
+	private Set<User> users;
+	
+	public UserManager() {
+		initialize();
+	}
+	
+	private void initialize() {
+		try {
+			users = new TreeSet<User>();
+			File file = new File("user-list");
+			file.createNewFile();
+
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = reader.readLine();
+			while (line != null) {
+				String[] credentials = line.split(":");
+				users.add(new User(credentials[0], credentials[1]));
+				line = reader.readLine();
+			}
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public boolean isUsernameUnique(String name) {
 		return false;
 	}
 
