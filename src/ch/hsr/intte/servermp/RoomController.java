889f895c2c397b1c481b7cf44a2603c4737b47d9
mrgimo <g.paulaitis@gmail.com>
2012-11-08 08:43:31 +0100

    implementing room view & controller

--- src/ch/hsr/intte/servermp/RoomController.java
+++ src/ch/hsr/intte/servermp/RoomController.java
@@ -1,29 +1,57 @@
 package ch.hsr.intte.servermp;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.push.PushContext;
+import org.primefaces.push.PushContextFactory;
+
+import ch.hsr.intte.servermp.model.Room;
+import ch.hsr.intte.servermp.model.User;
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class RoomController {
-	
+
+	private static final Lock lock = new ReentrantLock();
+
+	private User user;
+	private Room room;
+
+	private String message;
+
 	public RoomController() {
+		ChatSession chatSession = ChatSession.getInstance();
+
+		user = chatSession.getUser();
+		room = chatSession.getRoom();
+
+		user.enterRoom(room);
 	}
 
-	private String logout() {
-		System.out.println("Session invalidiert");
-		FacesContext fc = FacesContext.getCurrentInstance();
-		HttpSession session = (HttpSession) fc.getExternalContext().getSession(
-				false);
-		session.invalidate();
-		return "lobby.xhtml";
+	public void sendMessage() {
+		try {
+			lock.lock();
+			distributeMessage();
+		} finally {
+			lock.unlock();
+		}
 	}
 
-	public Integer getCount() {
-//		System.out.println(users == null);
-//		return users.size();
-		return null;
+	private void distributeMessage() {
+		PushContextFactory pushContextFactory = PushContextFactory.getDefault();
+		PushContext pushContext = pushContextFactory.getPushContext();
+
+		pushContext.push("/" + room.getName(), user.getUsername() + ": " + message);
 	}
+
+	public String leaveRoom() {
+		user.leaveRoom();
+		ChatSession.getInstance().invalidate();
+		return "login.xhtml";
+	}
+
 }
