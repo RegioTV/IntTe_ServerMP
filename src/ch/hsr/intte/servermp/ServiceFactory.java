30d20a5172a6d416f3e9747abf630c9b3662c960
mrgimo <g.paulaitis@gmail.com>
2012-11-08 09:25:28 +0100

    ApplicationFactory.getSession() deleted and renamed ApplicationFactory to ServiceFactory

--- src/ch/hsr/intte/servermp/ServiceFactory.java
+++ src/ch/hsr/intte/servermp/ServiceFactory.java
@@ -1,23 +1,19 @@
 package ch.hsr.intte.servermp;
 
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
+import ch.hsr.intte.servermp.services.RoomService;
+import ch.hsr.intte.servermp.services.UserService;
 
-public class ApplicationFactory {
+public class ServiceFactory {
 
 	private final static RoomService roomService = new RoomService();
 	private final static UserService userService = new UserService();
-	
+
 	public static RoomService getRoomService() {
 		return roomService;
 	}
-	
+
 	public static UserService getUserService() {
 		return userService;
 	}
-	
-	public static HttpSession getSession() {
-		FacesContext fc = FacesContext.getCurrentInstance();
-		return (HttpSession) fc.getExternalContext().getSession(false);
-	}
+
 }
