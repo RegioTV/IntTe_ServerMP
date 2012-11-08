c744b9ffcf4ef2b880c296bd99643c81182f3764
mrgimo <g.paulaitis@gmail.com>
2012-11-08 08:40:54 +0100

    class ChatSession added

---
+++ src/ch/hsr/intte/servermp/ChatSession.java
@@ -0,0 +1,51 @@
+package ch.hsr.intte.servermp;
+
+import java.util.Map;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import ch.hsr.intte.servermp.model.Room;
+import ch.hsr.intte.servermp.model.User;
+
+public class ChatSession {
+
+	private static final String USER = "user";
+	private static final String ROOM = "room";
+
+	private ChatSession() {}
+
+	public static ChatSession getInstance() {
+		return new ChatSession();
+	}
+
+	public User getUser() {
+		return (User) getSessionMap().get(USER);
+	}
+
+	public void setUser(User user) {
+		getSessionMap().put(USER, user);
+	}
+
+	public Room getRoom() {
+		return (Room) getSessionMap().get(ROOM);
+	}
+
+	public void setRoom(Room room) {
+		getSessionMap().put(ROOM, room);
+	}
+
+	private Map<String, Object> getSessionMap() {
+		return FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+	}
+
+	public void invalidate() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		HttpSession session = (HttpSession) externalContext.getSession(true);
+
+		session.invalidate();
+	}
+
+}
